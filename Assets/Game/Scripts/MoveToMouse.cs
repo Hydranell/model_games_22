88eb83d7ab63fe9a606280c40dd848e1d3c04d40
Hydranell <max.mueller@edu.fh-joanneum.at>
2022-03-24 23:47:18 +0100

    Started Editing V1 to V3

---
+++ Assets/Game/Scripts/MoveToMouse.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class MoveToMouse : MonoBehaviour
+{
+    public float maxMoveSpeed = 10;
+    public float smoothTime = 0.3f;
+    public float minDistance = 2;
+    Vector3 currentVelocity;
+    void Update()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // Offsets the target position so that the object keeps its distance.
+        mousePosition += ((Vector3)transform.position - mousePosition).normalized * minDistance;
+        transform.position = Vector3.SmoothDamp(transform.position, mousePosition, ref currentVelocity, smoothTime, maxMoveSpeed);
+    }
+}
+
+
+
+
+/*
+public class MoveToMouse : MonoBehaviour
+{
+    public float maxMoveSpeed = 10;
+    public float smoothTime = 0.3f;
+    public float minDistance = 2;
+    Vector3 currentVelocity;
+    void Update()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // Offsets the target position so that the object keeps its distance.
+        mousePosition += ((Vector3)transform.position - mousePosition).normalized * minDistance;
+        transform.position = Vector3.SmoothDamp(transform.position, mousePosition, ref currentVelocity, smoothTime, maxMoveSpeed);
+    }
+}
+
+*/
