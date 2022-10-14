75ade762cc4839e332e08411926bc9d7f7d1e4d4
Hydranell <max.mueller@edu.fh-joanneum.at>
2022-10-14 16:29:38 +0200

    Fucked With the damn Script
    and now it doesnt work

--- Assets/Game/Scripts/FollowTarget.cs
+++ Assets/Game/Scripts/FollowTarget.cs
@@ -7,6 +7,7 @@ public class FollowTarget : MonoBehaviour
 
     [SerializeField] private GameObject Target;
     [SerializeField] private GameObject Center;
+    [SerializeField] private float Radius;
     [SerializeField] private float speed = 1.5f;
 
 
@@ -20,9 +21,13 @@ public class FollowTarget : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, Target.transform.position, speed * Time.deltaTime);
+        Vector3 DisClo = Vector3.magnitude(Vector3(Target.transform.position - Center.transform.position) + Vector3.Normalize(Target.transform.position - Center.transform.position) * Radius * 1.25)
+           // TargetCenter + (Kugel-TargetCenter) + (Kugel-TargetCenter)normalize * Kugelradius * 1.25
+        
+           //old Moving Script:
+           //transform.position = Vector3.MoveTowards(transform.position, Target.transform.position, speed * Time.deltaTime);
 
- /// Here vektorstuff
-    
+        /// Here vektorstuff
+
     }
 }
