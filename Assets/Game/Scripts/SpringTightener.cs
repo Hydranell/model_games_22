b998e99109d9f97f877781074fdd1cc272460055
Hydranell <max.mueller@edu.fh-joanneum.at>
2022-11-04 11:47:13 +0100

    Started implementing new Script
    For Sping tightening

---
+++ Assets/Game/Scripts/SpringTightener.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringTightener : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (
+            //Collison mit Target == true
+            )
+        { 
+            GetComponent(SpringJoint).spring = 1000;
+        }
+    }
+}
