772d1cd5d17e1086007610598eee7c8182d9174d
Hydranell <max.mueller@edu.fh-joanneum.at>
2022-12-02 14:16:09 +0100

    Lev Did good things

--- Assets/Game/Scripts/MouseInteraction.cs
+++ Assets/Game/Scripts/MouseInteraction.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class MouseInteraction : MonoBehaviour
 {
     string lastHitName = "";
+    public GameObject orgpirate;
+    public Transform tGoal;
 
     // Start is called before the first frame update
     void Start()
@@ -19,15 +21,23 @@ public class MouseInteraction : MonoBehaviour
 
         var ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
 
-/*        if(Physics.Raycast(ray, out hit) && hit.rigidbody != null)
+        /*        if(Physics.Raycast(ray, out hit) && hit.rigidbody != null)
+                {
+
+                    //hit.rigidbody.AddForce(new Vector3(0, 2, 0), ForceMode.Impulse);
+                    hit.rigidbody.AddForceAtPosition(new Vector3(0, 2, 0),hit.point, ForceMode.Impulse);
+
+                }*/
+
+        if (Physics.Raycast(ray, out hit) && hit.transform.name == "FloorCube" && Input.GetMouseButtonDown(0))
         {
+            GameObject newPirate = GameObject.Instantiate<GameObject>(orgpirate, hit.point, Quaternion.identity);
+            ChangeColorClick[] ballScripts = GameObject.FindObjectsOfType<ChangeColorClick>();
+            newPirate.GetComponent<Bug>().tBall = ballScripts[Random.Range(0, ballScripts.Length)].gameObject.transform;
+            newPirate.GetComponent<Bug>().tGoal = tGoal;
+        }
 
-            //hit.rigidbody.AddForce(new Vector3(0, 2, 0), ForceMode.Impulse);
-            hit.rigidbody.AddForceAtPosition(new Vector3(0, 2, 0),hit.point, ForceMode.Impulse);
-
-        }*/
-
-        if (Physics.Raycast(ray, out hit) && hit.rigidbody != null && Input.GetMouseButtonDown(0))
+            if (Physics.Raycast(ray, out hit) && hit.rigidbody != null && Input.GetMouseButtonDown(0))
         {
 
             //hit.rigidbody.AddForce(new Vector3(0, 2, 0), ForceMode.Impulse);
